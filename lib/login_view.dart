ea8f02aa34dc762cc8e625041ee7f4bf4e707c66
BasakK6 <katukbasak@gmail.com>
2023-07-22 15:56:04 +0300

    feature: added widget tests and implemented UI for the login screen

---
+++ lib/login_view.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:test_driven_development/home_view.dart';
+import 'package:test_driven_development/validator.dart';
+
+class LoginView extends StatefulWidget {
+  const LoginView({Key? key}) : super(key: key);
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login Screen"),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  key: const ValueKey("email_text_form_field"),
+                  controller: _emailController,
+                  decoration: const InputDecoration(
+                    labelText: "Email",
+                  ),
+                  validator: (value) => Validator.validateEmail(value ?? ""),
+                ),
+                TextFormField(
+                    key: const ValueKey("password_text_form_field"),
+                    controller: _passwordController,
+                    decoration: const InputDecoration(
+                      labelText: "Password",
+                    ),
+                    validator: (value) =>
+                        Validator.validatePassword(value ?? "")),
+                const SizedBox(
+                  height: 16,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState?.validate() ?? false) {
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (context) => const HomeView()));
+                    }
+                  },
+                  child: const Text("Login"),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ test/login_view_test.dart
@@ -0,0 +1,190 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:test_driven_development/login_view.dart';
+
+void main(){
+  // TEST to FIND WIDGETS
+
+  testWidgets("Should have a title", (widgetTester) async {
+    //ARRANGE
+
+    //load the widget (this method returns a future so we should use await/async)
+    //wrap your desired widget with MaterialApp to have material design principles
+    await widgetTester.pumpWidget(const MaterialApp(home: LoginView()));
+
+    //ACT
+
+    //check if the title is there or not
+    Finder title = find.text("Login Screen");
+
+    //ASSERT
+    expect(title, findsOneWidget);
+  });
+
+  testWidgets("Should have one TextFormField to collect user email", (widgetTester) async {
+    //ARRANGE
+
+    //load the widget (this method returns a future so we should use await/async)
+    //wrap your desired widget with MaterialApp to have material design principles
+    await widgetTester.pumpWidget(const MaterialApp(home: LoginView()));
+
+    //ACT
+
+    //check if the email TextFormField is there or not
+    Finder emailTextFormField = find.byKey(const ValueKey("email_text_form_field"));
+
+    //ASSERT
+    expect(emailTextFormField, findsOneWidget);
+  });
+
+  testWidgets("Should have one TextFormField to collect user password", (widgetTester) async {
+    //ARRANGE
+
+    //load the widget (this method returns a future so we should use await/async)
+    //wrap your desired widget with MaterialApp to have material design principles
+    await widgetTester.pumpWidget(const MaterialApp(home: LoginView()));
+
+    //ACT
+
+    //check if the password TextFormField is there or not
+    Finder passwordTextFormField = find.byKey(const ValueKey("password_text_form_field"));
+
+    //ASSERT
+    expect(passwordTextFormField, findsOneWidget);
+  });
+
+  testWidgets("Should have one login button", (widgetTester) async {
+    //ARRANGE
+
+    //load the widget (this method returns a future so we should use await/async)
+    //wrap your desired widget with MaterialApp to have material design principles
+    await widgetTester.pumpWidget(const MaterialApp(home: LoginView()));
+
+    //ACT
+
+    //check if the login Button is there or not
+    Finder loginButton = find.byType(ElevatedButton);
+
+    //ASSERT
+    expect(loginButton, findsOneWidget);
+  });
+
+  // TEST the VALIDATION logic with WIDGETS
+
+  // test for the error case 1 -> empty input
+  testWidgets("Should show 'Required field' message if email & password is empty", (widgetTester) async {
+    //ARRANGE
+
+    //load the widget (this method returns a future so we should use await/async)
+    //wrap your desired widget with MaterialApp to have material design principles
+    await widgetTester.pumpWidget(const MaterialApp(home: LoginView()));
+
+    //ACT
+
+    //first find the login button and simulate the press action by tap()
+    Finder loginButton = find.byType(ElevatedButton);
+    await widgetTester.tap(loginButton);
+    //wait for the rendering complete after the action
+    await widgetTester.pumpAndSettle();
+
+    //find the required text after the action completed
+    Finder requiredFieldText = find.text("Required field");
+
+    //ASSERT
+    //there should be exactly 2 because we haven't provided input text for the TextFormFields
+    expect(requiredFieldText, findsNWidgets(2));
+  });
+
+  // test for the error case 2 -> invalid email
+  testWidgets("Should show 'Please enter a valid email' message if the entered email is in invalid format", (widgetTester) async {
+    //ARRANGE
+
+    //load the widget (this method returns a future so we should use await/async)
+    //wrap your desired widget with MaterialApp to have material design principles
+    await widgetTester.pumpWidget(const MaterialApp(home: LoginView()));
+
+    //ACT
+    //first, enter an invalid text to email TextFormField
+    Finder emailTextFormField = find.byKey(const ValueKey("email_text_form_field"));
+    await widgetTester.enterText(emailTextFormField, "invalid email example");
+
+    //second, find the login button and simulate the press action by tap()
+    Finder loginButton = find.byType(ElevatedButton);
+    await widgetTester.tap(loginButton);
+    //wait for the rendering complete after the action
+    await widgetTester.pumpAndSettle();
+
+    //find the required text after the action completed
+    Finder errorText = find.text("Please enter a valid email");
+
+    //ASSERT
+    //there should be exactly 2 because we haven't provided input text for the TextFormFields
+    expect(errorText, findsOneWidget);
+  });
+
+  // test for the error case 3 -> invalid password
+  testWidgets("Should show 'Password should be a minimum of 8 characters' if the entered password is less than 8 characters", (widgetTester) async {
+    //ARRANGE
+
+    //load the widget (this method returns a future so we should use await/async)
+    //wrap your desired widget with MaterialApp to have material design principles
+    await widgetTester.pumpWidget(const MaterialApp(home: LoginView()));
+
+    //ACT
+
+    //first, enter an invalid text to email TextFormField
+    Finder passwordTextFormField = find.byKey(const ValueKey("password_text_form_field"));
+    await widgetTester.enterText(passwordTextFormField, "12345");
+
+    //second, find the login button and simulate the press action by tap()
+    Finder loginButton = find.byType(ElevatedButton);
+    await widgetTester.tap(loginButton);
+    //wait for the rendering complete after the action
+    await widgetTester.pumpAndSettle();
+
+    //find the required text after the action completed
+    Finder errorText = find.text("Password should be a minimum of 8 characters");
+
+    //ASSERT
+
+    expect(errorText, findsOneWidget);
+  });
+
+  //test for the success scenario
+  testWidgets("Should show the HomeView when the user enters valid email & password and taps the login button", (widgetTester) async {
+    //ARRANGE
+
+    //load the widget (this method returns a future so we should use await/async)
+    //wrap your desired widget with MaterialApp to have material design principles
+    await widgetTester.pumpWidget(const MaterialApp(home: LoginView()));
+
+    //ACT
+
+    //first, find the TextFormField widgets and set valid texts
+    Finder emailTextFormField = find.byKey(const ValueKey("email_text_form_field"));
+    Finder passwordTextFormField = find.byKey(const ValueKey("password_text_form_field"));
+
+    await widgetTester.enterText(emailTextFormField, "dev.basakk6@gmail.com");
+    await widgetTester.enterText(passwordTextFormField, "12345678");
+
+    //second, find the login button and simulate the press action by tap()
+    Finder loginButton = find.byType(ElevatedButton);
+    await widgetTester.tap(loginButton);
+    //wait for the rendering complete after the action
+    await widgetTester.pumpAndSettle();
+
+    //find the required text after the action completed
+    Finder emptyFieldErrorText = find.text("Required field");
+    Finder emailErrorText = find.text("Please enter a valid email");
+    Finder passwordErrorText = find.text("Password should be a minimum of 8 characters");
+    Finder homeViewTitle = find.text("Home Screen");
+
+    //ASSERT
+
+    //there shouldn't be any error message and the route should change to HomeView
+    expect(emptyFieldErrorText, findsNothing);
+    expect(emailErrorText, findsNothing);
+    expect(passwordErrorText, findsNothing);
+    expect(homeViewTitle, findsOneWidget);
+  });
+}
